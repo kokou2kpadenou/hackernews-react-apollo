ffb72562fd41f2f4a2ee19c7aca81b6b4db65c28
kokou kpadenou <kkokou@gmail.com>
2019-08-13 22:11:57 -0400

    more mutations and updating the store

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -3,19 +3,42 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import Link from "./Link";
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   {
     feed {
       links {
         id
+        createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
 `;
 
 class LinkList extends Component {
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY });
+    console.log(data);
+
+    const votedLink = data.feed.links.find(link => link.id === linkId);
+    votedLink.votes = createVote.link.votes;
+
+    console.log(data);
+
+    store.writeQuery({ query: FEED_QUERY, data });
+  };
+
   render() {
     return (
       <Query query={FEED_QUERY}>
@@ -31,8 +54,13 @@ class LinkList extends Component {
           const linksToRender = data.feed.links;
           return (
             <div>
-              {linksToRender.map(link => (
-                <Link key={link.id} link={link} />
+              {linksToRender.map((link, index) => (
+                <Link
+                  key={link.id}
+                  link={link}
+                  index={index}
+                  updateStoreAfterVote={this._updateCacheAfterVote}
+                />
               ))}
             </div>
           );
